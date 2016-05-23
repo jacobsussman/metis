e3bf52603451e4d9732f24bfe31a8fcd004003bb
Eric Poretsky <eporetsky@wesleyan.edu>
2016-05-23 23:33:31 +0000

    Fixing db

--- db/schema.rb
+++ db/schema.rb
@@ -69,12 +69,11 @@ ActiveRecord::Schema.define(version: 20160519175430) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "company"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "coach"
+    t.boolean  "coach",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
