f6977bc5b471bd957179b52e83ae61c48c04c30e
Eric Poretsky <eporetsky@wesleyan.edu>
2016-05-23 23:52:46 +0000

    fixing db

--- db/migrate/20160518165851_add_coach_to_users.rb
+++ db/migrate/20160518165851_add_coach_to_users.rb
@@ -1,5 +1,5 @@
 class AddCoachToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :coach, :boolean, default: false
+    add_column :users, :coach, :boolean
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -69,11 +69,11 @@ ActiveRecord::Schema.define(version: 20160519175430) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "coach",           default: false
+    t.boolean  "coach"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
